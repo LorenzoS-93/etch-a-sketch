6c41093f1706bed1ef4f6319f6ba8dd32e09750a
LorenzoS-93 <180427301+LorenzoS-93@users.noreply.github.com>
2024-09-22 23:47:50 +0200

    feat: added random color added the ability to color with random color, small fix in the style

--- script.js
+++ script.js
@@ -8,6 +8,30 @@ content.setAttribute("class", "content");
 content.appendChild(container);
 container.setAttribute("class", "container");
 
+function rgbRandom() {
+    return Math.floor(Math.random() * 255);
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function colorSquare(e, color) {
+    if (color === "rainbow") {
+        e.setAttribute("class", `${e.getAttribute("class")} rainbow`);
+    }
+    else if (color === "random") {
+        const rnd = [rgbRandom(), rgbRandom(), rgbRandom()];
+        e.style.backgroundColor = `rgb(${rnd.toString()})`;
+    }
+    else {
+        e.style.backgroundColor = `${color}`;
+        //opa += 0.1;
+        //square.style.opacity = `${opa}`;
+    }
+
+}
+
 function game() {
     let color = "black";
     const clr = document.querySelector(".color");
@@ -33,47 +57,25 @@ function game() {
     for(const square of nodeList) {
         square.addEventListener("mousedown", () => {
             isDrawing = true
-            if (color === "rainbow"){
-                square.setAttribute("class", `${square.getAttribute("class")} rainbow`);
-            }
-            else {
-                square.style.backgroundColor = `${color}`;
-                //opa += 0.1;
-                //square.style.opacity = `${opa}`;
-            }
-                
+            colorSquare(square, color);
         });
         square.addEventListener("mousemove", () => {
             if(isDrawing) {
-                if (color === "rainbow"){
-                    square.setAttribute("class", `${square.getAttribute("class")} rainbow`);
-                }
-                else {
-                    square.style.backgroundColor = `${color}`;
-                    //opa += 0.1;
-                    //square.style.opacity = `${opa}`;
-                }
+                colorSquare(square, color);
             } 
         });
         square.addEventListener("mouseup", () => {
             if(isDrawing) {
-                if (color === "rainbow"){
-                    square.setAttribute("class", `${square.getAttribute("class")} rainbow`);
-                }
-                else {
-                    square.style.backgroundColor = `${color}`;
-                    //opa += 0.1;
-                    //square.style.opacity = `${opa}`;
-                }
+                colorSquare(square, color);
                 isDrawing = false;
             }
         });
     }
-    
-    
+      
     clr.addEventListener("click", () => {
-    color = prompt("Enter desired color", "black");
-    clr.style.backgroundColor = `${color}`;
+        color = prompt("Enter desired color", "black");
+        colorSquare(clr, color);
+        clr.textContent = `${capitalizeFirstLetter(color.toLowerCase())}`;
     });
 }
 
