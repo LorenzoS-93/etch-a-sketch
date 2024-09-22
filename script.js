b51b016e1ce86998809a6169920cbd28aa08ba0b
LorenzoS-93 <180427301+LorenzoS-93@users.noreply.github.com>
2024-09-22 21:19:01 +0200

    feat: added a colour button the colour button lets you change the colour of the "pen"

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ content.setAttribute("class", "content");
 content.appendChild(container);
 container.setAttribute("class", "container");
 
+
+
 function game() {
+    let colour = "black";
     // create n*n square in a grid;
     while (container.firstChild) {
         container.removeChild(container.lastChild);
@@ -29,25 +32,41 @@ function game() {
     for(const square of nodeList) {
         square.addEventListener("mousedown", () => {
             isDrawing = true;
-            square.setAttribute("class", `${square.getAttribute("class")} colored`);
+            //square.setAttribute("class", `${square.getAttribute("class")} ${colour}`);
+            square.style.backgroundColor = `${colour}`;
         });
         square.addEventListener("mousemove", () => {
-            if(isDrawing) square.setAttribute("class", `${square.getAttribute("class")} colored`);
+            if(isDrawing) {
+                //square.setAttribute("class", `${square.getAttribute("class")} ${colour}`);
+                square.style.backgroundColor = `${colour}`;
+            } 
         });
         square.addEventListener("mouseup", () => {
             if(isDrawing) {
-                square.setAttribute("class", `${square.getAttribute("class")} colored`);
+                //square.setAttribute("class", `${square.getAttribute("class")} ${colour}`);
+                square.style.backgroundColor = `${colour}`;
                 isDrawing = false;
-            } 
+            }
         });
     }
-
+    
+    const clr = document.querySelector(".colour");
+    clr.addEventListener("click", () => {
+    colour = prompt("Enter desired colour", "black");
+});
 }
 
 game();
 
-const btn = document.querySelector("button");
+
+
+
+const btn = document.querySelector(".reset");
 btn.addEventListener("click", () => {
-    n = prompt("Enter the number of square", "maxx 100");
+    n = prompt("Enter the lenght of the square (max 100)", "number");
+    while (n <= 0 || n >100) {
+        alert("Invalid choice, insert a number bigger than 0 and smaller or equal to 100");
+        n = prompt("Enter the lenght of the square (max 100)", "16");
+    }
     game();
 });
